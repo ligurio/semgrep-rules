f6d179ec0cf144a0ab7123e564e77247007749ce
Sergey Bronnikov <sergeyb@tarantool.org>
2021-03-01 18:21:48 +0300

    Update fix sections

--- rules/non-determinism/python/use-current-time.py
+++ rules/non-determinism/python/use-current-time.py
@@ -1,6 +1,7 @@
 import random
 import time
 from datetime import datetime
+from timeit import timeit
 
 t = datetime.now()
 # rule-id: use-current-time
@@ -33,3 +34,8 @@ assert(t <= 0)
 assert(t <> 0)
 # rule-id: use-current-time
 assert(t != 0)
+# rule-id: use-current-time
+time.clock()
+
+# rule-id: use-current-time
+a = timeit.timeit()
