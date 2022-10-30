eac171ad3cf60fb8dc7d54f268c70211faa87cb5
Sergey Bronnikov <estetus@gmail.com>
2022-10-30 20:57:11 +0300

    rules: fix errors on testing

---
+++ rules/python/magic_number.py
@@ -0,0 +1,17 @@
+i = 2
+f = 0.2
+s = "hello"
+
+# ruleid: magic_number
+assert(4 == i)
+# ruleid: magic_number
+assert(4 == f)
+# ruleid: magic_number
+assert(4 == s)
+
+# ruleid: magic_number
+assert(i == 4)
+# ruleid: magic_number
+assert(f == 4)
+# ruleid: magic_number
+assert(s == 4)
