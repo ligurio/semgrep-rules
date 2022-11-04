0341b00bb36bd82dc7b7aed9d6d5d6ffd61da868
Sergey Bronnikov <estetus@gmail.com>
2022-11-04 17:50:38 +0300

    rules/lua: add unsafe_function rule

---
+++ rules/lua/basic/unsafe_function.lua
@@ -0,0 +1,17 @@
+-- ruleid: unsafe_function
+os.execute("")
+
+-- ruleid: unsafe_function
+load("")
+
+-- ruleid: unsafe_function
+loadstring("")
+
+-- ruleid: unsafe_function
+loadfile("")
+
+-- ruleid: unsafe_function
+dofile("")
+
+-- ruleid: unsafe_function
+os.remove("")
