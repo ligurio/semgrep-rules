3382df6f682dd7ab880ab82a7479e4f3958efbd4
Sergey Bronnikov <estetus@gmail.com>
2022-11-04 20:47:00 +0300

    rules/python: fix sync_sleep rule

--- rules/python/flakiness/sync_sleep.py
+++ rules/python/flakiness/sync_sleep.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+import asyncio
+
 # rule-id: sync_sleep
 webdriver.set_page_load_timeout(30)
 webdriver.set_page_load_timeout(0.3)
@@ -7,20 +9,13 @@ webdriver.set_page_load_timeout(0.3)
 webdriver.set_script_timeout(20)
 webdriver.set_script_timeout(0.2)
 
-# rule-id: sync_sleep
-await asyncio.sleep(1)
+async def foo():
+    # rule-id: sync_sleep
+    await asyncio.sleep(1)
 
 import time
+
 # rule-id: sync_sleep
 time.sleep(10)
 # rule-id: sync_sleep
 time.sleep(0.2)
-
-def a():
-    return 10
-
-# ok: sync_sleep
-t.sleep(a())
-
-# ok: sync_sleep
-t.sleep(some_var)
