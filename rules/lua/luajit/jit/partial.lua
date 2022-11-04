e399ad849826b9a984be4ce2525f27799e126ddc
Sergey Bronnikov <estetus@gmail.com>
2022-11-04 21:20:07 +0300

    rules/luajit: add partial rule

---
+++ rules/lua/luajit/jit/partial.lua
@@ -0,0 +1,46 @@
+-- ruleid: partial
+getfenv()
+
+-- ruleid: partial
+next(...)
+
+-- ruleid: partial
+print(...)
+
+-- ruleid: partial
+select(...)
+
+-- ruleid: partial
+tonumber(...)
+
+-- ruleid: partial
+tostring(...)
+
+-- ruleid: partial
+xpcall(...)
+
+-- ruleid: partial
+string.find(...)
+
+-- ruleid: partial
+string.format(...)
+
+-- ruleid: partial
+table.insert(...)
+
+local ffi = require("ffi")
+
+-- ruleid: partial
+ffi.cast(...)
+
+-- ruleid: partial
+ffi.errno(...)
+
+-- ruleid: partial
+ffi.new(...)
+
+-- ruleid: partial
+ffi.sizeof(...)
+
+-- ruleid: partial
+ffi.typeof(...)
