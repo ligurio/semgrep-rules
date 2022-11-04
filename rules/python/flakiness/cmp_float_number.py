ff2f51aedfd134574c19c916dfa7199e37677c26
Sergey Bronnikov <estetus@gmail.com>
2022-11-04 20:45:04 +0300

    rules/python: fix cmp_float_number

--- rules/python/flakiness/cmp_float_number.py
+++ rules/python/flakiness/cmp_float_number.py
@@ -2,13 +2,31 @@ m = 5
 n = 0.000001
 
 # rule-id: cmp_float_numbers
-assert(1.0000 == 1.000000)
+assert(n == 1.000000)
 
 # rule-id: cmp_float_numbers
-assert(1.0000 == m)
-
-# rule-id: cmp_float_numbers
-assert(m == 1.0000)
+assert(6.0001 == m)
 
 # rule-id: cmp_float_numbers
 assert(m == n)
+
+# rule-id: cmp_float_numbers
+assert(3. == n)
+# rule-id: cmp_float_numbers
+assert(3.3 == n)
+# rule-id: cmp_float_numbers
+assert(3.3e2 == n)
+# rule-id: cmp_float_numbers
+assert(3.3e-1 == n)
+# rule-id: cmp_float_numbers
+assert(.1e1 == n)
+# rule-id: cmp_float_numbers
+assert(1.11e12 == n)
+# rule-id: cmp_float_numbers
+assert(1.3e-1 == n)
+# rule-id: cmp_float_numbers
+assert(.1e1 == n)
+# rule-id: cmp_float_numbers
+assert(.2e-14 == n)
+# rule-id: cmp_float_numbers
+assert(5E6 == n)
