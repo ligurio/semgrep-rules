37dab5a9426c523eba3fd809c3d1c4baf8801a9c
Sergey Bronnikov <estetus@gmail.com>
2026-09-24 19:41:46 +0300

    python: fix flakiness tests
    Fix test annotations for the Python flakiness rules so semgrep
    --test passes (wrong annotation syntax, annotations on the wrong
    line).

--- rules/python/flakiness/missed_retries.py
+++ rules/python/flakiness/missed_retries.py
@@ -4,22 +4,24 @@ import requests
 url = "https://bronevichok.ru/"
 
 s = requests.Session()
-# rule-id: missed_retries
+# ruleid: missed_retries
 s.mount(url, HTTPAdapter())
 
-# ok: missed_retries
+# ruleid: missed_retries
 s.mount(url, HTTPAdapter(max_retries=3))
 
 from urllib3 import PoolManager, Retry
+# ruleid: missed_retries
 http = PoolManager()
-# rule-id: missed_retries
 response = http.request('GET', url)
 
 retries = Retry(connect=5, read=2, redirect=5)
+# ruleid: missed_retries
 http = PoolManager(retries=retries)
 # ok: missed_retries
 response = http.request('GET', url)
 
+# ruleid: missed_retries
 http = PoolManager()
 # ok: missed_retries
 response = http.request('GET', url, retries=Retry(10))
--- rules/python/flakiness/sync_sleep.py
+++ rules/python/flakiness/sync_sleep.py
@@ -1,21 +1,23 @@
 from selenium import webdriver
 import asyncio
 
-# rule-id: sync_sleep
+# ruleid: sync_sleep
 webdriver.set_page_load_timeout(30)
+# ruleid: sync_sleep
 webdriver.set_page_load_timeout(0.3)
 
-# rule-id: sync_sleep
+# ruleid: sync_sleep
 webdriver.set_script_timeout(20)
+# ruleid: sync_sleep
 webdriver.set_script_timeout(0.2)
 
 async def foo():
-    # rule-id: sync_sleep
+    # ruleid: sync_sleep
     await asyncio.sleep(1)
 
 import time
 
-# rule-id: sync_sleep
+# ruleid: sync_sleep
 time.sleep(10)
-# rule-id: sync_sleep
+# ruleid: sync_sleep
 time.sleep(0.2)
