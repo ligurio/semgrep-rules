feed89a6153d44cabf9334c54f83f9e252640300
Sergey Bronnikov <estetus@gmail.com>
2026-09-25 13:30:25 +0300

    rules/c: rename rules and move Lua C API rules to rules/c
    - Move rules/c/lua-c-api/* to rules/c/
    - Prefix Lua C API rule IDs with lua_capi_
    - Rename clock-bugs to libc_clock
    - Update README rules table

--- rules/c/lua_capi_missing_type_check.c
+++ rules/c/lua_capi_missing_type_check.c
@@ -7,7 +7,7 @@ static void use_ptr(void *p);
 
 void unchecked_string(lua_State *L)
 {
-	// ruleid: lua_missing_type_check
+	// ruleid: lua_capi_missing_type_check
 	const char *s = lua_tostring(L, 1);
 	use(s);
 }
@@ -15,7 +15,7 @@ void unchecked_string(lua_State *L)
 void guarded_by_isstring(lua_State *L)
 {
 	if (lua_isstring(L, 1)) {
-		// ok: lua_missing_type_check
+		// ok: lua_capi_missing_type_check
 		use(lua_tostring(L, 1));
 	}
 }
@@ -24,7 +24,7 @@ void guarded_by_early_return(lua_State *L)
 {
 	if (!lua_isstring(L, 1))
 		return;
-	// ok: lua_missing_type_check
+	// ok: lua_capi_missing_type_check
 	const char *s = lua_tostring(L, 1);
 	use(s);
 }
@@ -32,7 +32,7 @@ void guarded_by_early_return(lua_State *L)
 void guarded_by_lua_type(lua_State *L)
 {
 	if (lua_type(L, 1) == LUA_TSTRING) {
-		// ok: lua_missing_type_check
+		// ok: lua_capi_missing_type_check
 		use(lua_tostring(L, 1));
 	}
 }
@@ -40,7 +40,7 @@ void guarded_by_lua_type(lua_State *L)
 void guarded_by_checkstring(lua_State *L)
 {
 	luaL_checkstring(L, 1);
-	// ok: lua_missing_type_check
+	// ok: lua_capi_missing_type_check
 	const char *s = lua_tostring(L, 1);
 	use(s);
 }
@@ -49,14 +49,14 @@ void unchecked_wrong_index(lua_State *L)
 {
 	if (lua_isstring(L, 1))
 		use(NULL);
-	// ruleid: lua_missing_type_check
+	// ruleid: lua_capi_missing_type_check
 	const char *s = lua_tostring(L, 2);
 	use(s);
 }
 
 void unchecked_number(lua_State *L)
 {
-	// ruleid: lua_missing_type_check
+	// ruleid: lua_capi_missing_type_check
 	lua_Integer n = lua_tointeger(L, 2);
 	use_int(n);
 }
@@ -64,14 +64,14 @@ void unchecked_number(lua_State *L)
 void guarded_number(lua_State *L)
 {
 	if (lua_isnumber(L, 2)) {
-		// ok: lua_missing_type_check
+		// ok: lua_capi_missing_type_check
 		use_int(lua_tointeger(L, 2));
 	}
 }
 
 void unchecked_userdata(lua_State *L)
 {
-	// ruleid: lua_missing_type_check
+	// ruleid: lua_capi_missing_type_check
 	void *p = lua_touserdata(L, 1);
 	use_ptr(p);
 }
@@ -79,14 +79,14 @@ void unchecked_userdata(lua_State *L)
 void guarded_userdata(lua_State *L)
 {
 	if (luaL_testudata(L, 1, "mytype")) {
-		// ok: lua_missing_type_check
+		// ok: lua_capi_missing_type_check
 		use_ptr(lua_touserdata(L, 1));
 	}
 }
 
 void unchecked_getfield(lua_State *L)
 {
-	// ruleid: lua_missing_type_check
+	// ruleid: lua_capi_missing_type_check
 	lua_getfield(L, 1, "key");
 	lua_pop(L, 1);
 }
@@ -94,7 +94,7 @@ void unchecked_getfield(lua_State *L)
 void guarded_getfield_checktype(lua_State *L)
 {
 	luaL_checktype(L, 1, LUA_TTABLE);
-	// ok: lua_missing_type_check
+	// ok: lua_capi_missing_type_check
 	lua_getfield(L, 1, "key");
 	lua_pop(L, 1);
 }
@@ -102,7 +102,7 @@ void guarded_getfield_checktype(lua_State *L)
 void guarded_getfield_istable(lua_State *L)
 {
 	if (lua_istable(L, 1)) {
-		// ok: lua_missing_type_check
+		// ok: lua_capi_missing_type_check
 		lua_getfield(L, 1, "key");
 		lua_pop(L, 1);
 	}
@@ -110,13 +110,13 @@ void guarded_getfield_istable(lua_State *L)
 
 void not_an_api_conversion(lua_State *L)
 {
-	// ok: lua_missing_type_check
+	// ok: lua_capi_missing_type_check
 	lua_pushstring(L, "hello");
 }
 
 void checkstring_without_to(lua_State *L)
 {
-	// ok: lua_missing_type_check
+	// ok: lua_capi_missing_type_check
 	const char *s = luaL_checkstring(L, 1);
 	use(s);
 }
--- rules/c/lua_capi_pcall_result_unused.c
+++ rules/c/lua_capi_pcall_result_unused.c
@@ -3,19 +3,19 @@
 
 void ignores_pcall(lua_State *L)
 {
-	// ruleid: lua_pcall_result_unused
+	// ruleid: lua_capi_pcall_result_unused
 	lua_pcall(L, 0, 0, 0);
 }
 
 void ignores_dostring(lua_State *L)
 {
-	// ruleid: lua_pcall_result_unused
+	// ruleid: lua_capi_pcall_result_unused
 	luaL_dostring(L, "return 1");
 }
 
 void checks_pcall(lua_State *L)
 {
-	// ok: lua_pcall_result_unused
+	// ok: lua_capi_pcall_result_unused
 	if (lua_pcall(L, 0, 0, 0) != LUA_OK) {
 		return;
 	}
@@ -25,13 +25,13 @@ void stores_pcall(lua_State *L)
 {
 	int rc;
 
-	// ok: lua_pcall_result_unused
+	// ok: lua_capi_pcall_result_unused
 	rc = lua_pcall(L, 0, 0, 0);
 	(void)rc;
 }
 
 void explicit_ignore(lua_State *L)
 {
-	// ok: lua_pcall_result_unused
+	// ok: lua_capi_pcall_result_unused
 	(void)lua_pcall(L, 0, 0, 0);
 }
