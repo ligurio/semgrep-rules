4f6ccdfd98c11975a24cf866eee85f69d890391f
Sergey Bronnikov <estetus@gmail.com>
2026-09-25 14:11:50 +0300

    rules/lua: do not report io.write and io.flush in T1032
    Writes to stdout are not replaced by fio and are common in test
    harnesses, e.g. lua/tap.lua. Keep the rule focused on file I/O.
    
    Add a negative test case for io.write.

--- rules/lua/tarantool/T1032.lua
+++ rules/lua/tarantool/T1032.lua
@@ -5,3 +5,7 @@ local fh = io.open("/var/log/app.log", "r")
 
 -- # ok: T1032
 local fh2 = fio.open("/var/log/app.log", {"O_RDONLY"})
+
+-- Writing to stdout is not replaced by fio.
+-- # ok: T1032
+io.write("hello\n")
